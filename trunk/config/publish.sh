224058c4740b333789c283d218e6c1fa2045516b
filip.jurcicek@gmail.com <filip.jurcicek@gmail.com@a7b0ce2d-bf3e-712c-ee01-dc7b9fe4d7da>
2011-07-22 20:45:29 +0000

    Update to the configuration procedure and the documentation.

--- trunk/config/publish.sh
+++ trunk/config/publish.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-# ping.zou 2010-07-20
 
-WEB_ROOT="/home/zzzzz/public_html/zzzzzzzzzzz/cturk-transcriber/wwwroot";
-DOC_ROOT="/home/zzzzz/public_html/zzzzzzzzzzz/cturk-transcriber/docs";
+WEB_ROOT="/home/zzzzz/src/zzzzzzzzzzz/cturk-transcriber/wwwroot";
+DOC_ROOT="/home/zzzzz/src/zzzzzzzzzzz/cturk-transcriber/docs";
 
 rm -r $WEB_ROOT/data/wav/`basename $1`;
 mkdir $WEB_ROOT/data/wav/`basename $1`;
